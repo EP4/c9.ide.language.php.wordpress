05606e0f5f6526b087d7376eddb8cc3c0bc4bc84
Dave Lavoie <dave.lavoie@ep4.com>
2017-02-10 22:45:02 +0000

    Adding the Cloud9 plugin to the repo.

---
+++ phpcs.wpcs.js
@@ -0,0 +1,55 @@
+define(function(require, exports, module) {
+    main.consumes = ["Plugin", "language","ui"];
+    main.provides = ["phpcs.wpcs"];
+    return main;
+
+    function main(options, imports, register) {
+        var Plugin = imports.Plugin;
+        
+        /***** Initialization *****/
+        var plugin = new Plugin("EP4.com", main.consumes);
+        var language = imports.language;
+        var emit = plugin.getEmitter();
+        var ui = imports.ui;
+
+        function load() {
+            var path = options.packagePath.substr(0, options.packagePath.lastIndexOf("/"));
+            
+            // Load PHPCS
+            language.registerLanguageHandler(
+                path + "/worker/phpcs_handler",
+                function(err, handler) {
+                    if (err) return err;
+                    // send/receive events using handler.emit/on
+                },
+                plugin
+            );
+            
+            // Load custom CSS stylesheet.
+            ui.insertCss(require("text!./style.css"), options.staticPrefix, plugin);
+        }
+        
+        /***** Methods *****/
+        
+        
+        
+        /***** Lifecycle *****/
+        
+        plugin.on("load", function() {
+           load();
+        });
+        plugin.on("unload", function() {
+        
+        });
+        
+        /***** Register and define API *****/
+        
+        plugin.freezePublicAPI({
+            
+        });
+        
+        register(null, {
+            "phpcs.wpcs": plugin
+        });
+    }
+});
